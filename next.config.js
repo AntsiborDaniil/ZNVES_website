abb599b3e8365bf62a2d4ca9ed831323b2410eeb
ancibordaniil <130238809+ancibordaniil@users.noreply.github.com>
2025-11-11 23:36:29 +0300

    style: отредактированы стили footer

--- next.config.js
+++ next.config.js
@@ -1,57 +1,60 @@
 const isDev = process.env.NODE_ENV !== "production";
 
 const cspDirectives = {
-    "default-src": ["'self'"],
-    "script-src": ["'self'"],
-    "style-src": ["'self'", "'unsafe-inline'"],
-    "img-src": ["'self'", "data:"],
-    "font-src": ["'self'", "data:"],
-    "connect-src": ["'self'"],
-    "frame-ancestors": ["'self'"],
+  "default-src": ["'self'"],
+  "script-src": ["'self'"],
+  "style-src": ["'self'", "'unsafe-inline'"],
+  "img-src": ["'self'", "data:"],
+  "font-src": ["'self'", "data:"],
+  "connect-src": ["'self'"],
+  "frame-ancestors": ["'self'"],
 };
 
 if (isDev) {
-    cspDirectives["script-src"].push("'unsafe-inline'", "'unsafe-eval'");
+  cspDirectives["script-src"].push("'unsafe-inline'", "'unsafe-eval'");
 }
 
 const cspHeader = Object.entries(cspDirectives)
-    .map(([directive, values]) => `${directive} ${values.join(" ")}`)
-    .join("; ");
+  .map(([directive, values]) => `${directive} ${values.join(" ")}`)
+  .join("; ");
 
 const securityHeaders = [
-    {
-        key: "Content-Security-Policy",
-        value: cspHeader,
-    },
-    {
-        key: "Referrer-Policy",
-        value: "strict-origin-when-cross-origin",
-    },
-    {
-        key: "X-Content-Type-Options",
-        value: "nosniff",
-    },
-    {
-        key: "X-Frame-Options",
-        value: "SAMEORIGIN",
-    },
-    {
-        key: "X-XSS-Protection",
-        value: "1; mode=block",
-    },
+  {
+    key: "Content-Security-Policy",
+    value: cspHeader,
+  },
+  {
+    key: "Referrer-Policy",
+    value: "strict-origin-when-cross-origin",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
 ];
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    async headers() {
-        return [
-            {
-                source: "/(.*)",
-                headers: securityHeaders,
-            },
-        ];
-    },
+  reactStrictMode: true,
+  images: {
+    qualities: [75, 80, 85, 90, 95, 100],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
