34be6326df65acb893724839c904fdd482a3a59f
Antsibor Daniil <s1lence0073777@gmail.com>
2025-11-24 17:52:35 +0300

    style: мой кабинет стилизован

--- src/components/AccountPage/MyAccount/MyAccount.tsx
+++ src/components/AccountPage/MyAccount/MyAccount.tsx
@@ -4,108 +4,148 @@ import Image from "next/image";
 import styles from "./MyAccount.module.css";
 
 type MyAccountProps = {
-  onNavigate?: (tab: "profile" | "orders") => void;
+    onNavigate?: (tab: "profile" | "orders") => void;
 };
 
 const MyAccount = ({ onNavigate }: MyAccountProps) => {
-  const handlePersonalDataClick = () => {
-    onNavigate?.("profile");
-  };
+    const handlePersonalDataClick = () => {
+        onNavigate?.("profile");
+    };
 
-  const handleOrdersClick = () => {
-    onNavigate?.("orders");
-  };
+    const handleOrdersClick = () => {
+        onNavigate?.("orders");
+    };
 
-  return (
-    <div className={styles.container}>
-      <div
-        className={styles.card}
-        onClick={handlePersonalDataClick}
-        role="button"
-        tabIndex={0}
-        onKeyDown={(e) => {
-          if (e.key === "Enter" || e.key === " ") {
-            e.preventDefault();
-            handlePersonalDataClick();
-          }
-        }}
-      >
-        <div className={styles.cardContent}>
-          <div className={styles.cardHeader}>Личные данные</div>
-          <div className={styles.cardTitle}>
-            Смирнов Александр Александрович
-          </div>
-          <div className={styles.cardInfo}>
-            <div className={styles.cardInfoItem}>abvgd@mail.co.m</div>
-            <div className={styles.cardInfoItem}>+7 (977) 721-04-52</div>
-          </div>
-        </div>
-        <div className={styles.cardArrow}>
-          <Image
-            src="/images/arrow-right.png"
-            alt="Перейти"
-            width={24}
-            height={24}
-          />
-        </div>
-      </div>
+    return (
+        <div className={styles.container}>
+            <div
+                className={styles.card}
+                onClick={handlePersonalDataClick}
+                role="button"
+                tabIndex={0}
+                onKeyDown={(e) => {
+                    if (e.key === "Enter" || e.key === " ") {
+                        e.preventDefault();
+                        handlePersonalDataClick();
+                    }
+                }}
+            >
+                <div className={styles.cardContent}>
+                    <div className={styles.cardContentInner}>
+                        <div className={styles.cardContentInnerLeft}>
+                            <h1 className={styles.cardHeader}>Личные данные</h1>
+                            <h1 className={styles.cardTitle}>
+                                Смирнов Александр Александрович
+                            </h1>
+                        </div>
+                        <Image
+                            src="/images/account/arrowRight.png"
+                            alt="Стрелка"
+                            width={41}
+                            height={39}
+                            className={styles.thumbnailImageArrow}
+                        />
+                    </div>
+                    <div className={styles.cardInfo}>
+                        <div className={styles.cardInfoItem}>
+                            abvgd@mail.com
+                        </div>
+                        <div className={styles.cardInfoItem}>
+                            +7 (977) 721-04-52
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.cardArrow}>
+                    <Image
+                        src="/images/arrow-right.png"
+                        alt="Перейти"
+                        width={24}
+                        height={24}
+                    />
+                </div>
+            </div>
 
-      <div
-        className={styles.card}
-        onClick={handleOrdersClick}
-        role="button"
-        tabIndex={0}
-        onKeyDown={(e) => {
-          if (e.key === "Enter" || e.key === " ") {
-            e.preventDefault();
-            handleOrdersClick();
-          }
-        }}
-      >
-        <div className={styles.cardContent}>
-          <div className={styles.cardHeader}>Активные заказы</div>
-          <div className={styles.cardTitle}>Заказ от 24.09.2025</div>
-          <div className={styles.orderDetails}>
-            <div className={styles.orderNumber}>№456378495</div>
-            <div className={styles.orderStatus}>Ожидает оплаты</div>
-            <div className={styles.orderState}>
-              <span>•</span> Новый (<u>не оплачен</u>)
+            <div
+                className={styles.card}
+                onClick={handleOrdersClick}
+                role="button"
+                tabIndex={0}
+                onKeyDown={(e) => {
+                    if (e.key === "Enter" || e.key === " ") {
+                        e.preventDefault();
+                        handleOrdersClick();
+                    }
+                }}
+            >
+                <div className={styles.cardContent}>
+                    <div className={styles.cardContentInner}>
+                        <div className={styles.cardContentInnerLeft}>
+                            <h1 className={styles.cardHeader}>
+                                Активные заказы
+                            </h1>
+                            <h1 className={styles.cardTitle}>
+                                Заказ от 24.09.2025
+                            </h1>
+                        </div>
+                        <Image
+                            src="/images/account/arrowRight.png"
+                            alt="Стрелка"
+                            width={41}
+                            height={39}
+                            className={styles.thumbnailImageArrow}
+                        />
+                    </div>
+                    <div className={styles.orderDetailsContainer}>
+                        <div className={styles.orderDetailsLeft}>
+                            <div className={styles.orderDetailsTop}>
+                                <div className={styles.cardInfoItem}>
+                                    №456378495
+                                </div>
+                                <div className={styles.orderStatus}>
+                                    Ожидает оплаты
+                                </div>
+                            </div>
+                            <div className={styles.orderDetailsBottom}>
+                                <div className={styles.orderState}>
+                                    <span>•</span> <h2 className={styles.orderStateText}>Новый (не оплачен)</h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.orderDetailsRight}>
+                            <div className={styles.orderThumbnails}>
+                                <div className={styles.thumbnail}>
+                                    <Image
+                                        src="/images/catalogs/voyage.png"
+                                        alt="Товар"
+                                        width={60}
+                                        height={60}
+                                        className={styles.thumbnailImage}
+                                    />
+                                </div>
+                                <div className={styles.thumbnail}>
+                                    <Image
+                                        src="/images/catalogs/voyage.png"
+                                        alt="Товар"
+                                        width={60}
+                                        height={60}
+                                        className={styles.thumbnailImage}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.cardArrow}>
+                    <Image
+                        src="/images/arrow-right.png"
+                        alt="Перейти"
+                        width={24}
+                        height={24}
+                    />
+                </div>
             </div>
-          </div>
         </div>
-        <div className={styles.cardRight}>
-          <div className={styles.orderThumbnails}>
-            <div className={styles.thumbnail}>
-              <Image
-                src="/images/catalogs/voyage.png"
-                alt="Товар"
-                width={60}
-                height={60}
-                className={styles.thumbnailImage}
-              />
-            </div>
-            <div className={styles.thumbnail}>
-              <Image
-                src="/images/catalogs/voyage.png"
-                alt="Товар"
-                width={60}
-                height={60}
-                className={styles.thumbnailImage}
-              />
-            </div>
-          </div>
-          <div className={styles.cardArrow}>
-            <Image
-              src="/images/arrow-right.png"
-              alt="Перейти"
-              width={24}
-              height={24}
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default MyAccount;
