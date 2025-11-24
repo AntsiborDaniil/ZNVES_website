68fd83ae380941531d1edb7545f8e792b03b942c
Antsibor Daniil <s1lence0073777@gmail.com>
2025-11-24 20:52:28 +0300

    feat: переносит на страницу аккаунта

--- src/components/ui/AccountIcon/AccountIcon.tsx
+++ src/components/ui/AccountIcon/AccountIcon.tsx
@@ -5,28 +5,28 @@ import { useRouter } from "next/navigation";
 import styles from "./AccountIcon.module.css";
 
 const AccountIcon = () => {
-  const router = useRouter();
+    const router = useRouter();
 
-  const handleClick = () => {
-    router.push("/register");
-  };
+    const handleClick = () => {
+        router.push("/account");
+    };
 
-  return (
-    <button
-      className={styles.accountIcon}
-      aria-label="Личный кабинет"
-      type="button"
-      onClick={handleClick}
-    >
-      <Image
-        src="/images/account.png"
-        alt="Личный кабинет"
-        width={38}
-        height={38}
-        className={styles.accountImage}
-      />
-    </button>
-  );
+    return (
+        <button
+            className={styles.accountIcon}
+            aria-label="Личный кабинет"
+            type="button"
+            onClick={handleClick}
+        >
+            <Image
+                src="/images/account.png"
+                alt="Личный кабинет"
+                width={38}
+                height={38}
+                className={styles.accountImage}
+            />
+        </button>
+    );
 };
 
 export default AccountIcon;
