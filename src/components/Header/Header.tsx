2441a2da7c9fb0730a244065b34f47880b56e4f3
Antsibor Daniil <s1lence0073777@gmail.com>
2026-02-14 17:27:20 +0300

    fix: убрано агрессивное монтирование некоторых страниц

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState, Suspense } from "react";
+import { usePathname } from "next/navigation";
 import BurgerMenu from "../BurgerMenu/BurgerMenu";
 import CartIcon from "../ui/CartIcon/CartIcon";
 import AccountIcon from "../ui/AccountIcon/AccountIcon";
@@ -16,6 +17,8 @@ type HeaderProps = {
 
 const Header = ({ variant = "transparent" }: HeaderProps) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const pathname = usePathname();
+  const shouldPrefetch = pathname !== "/checkout" && pathname !== "/cart";
 
   const headerClassName = `${styles.header} ${
     variant === "green" ? styles.headerGreen : ""
@@ -31,7 +34,12 @@ const Header = ({ variant = "transparent" }: HeaderProps) => {
           />
         </Suspense>
       </div>
-      <Link href="/" className={styles.logoLink} aria-label="Go to homepage">
+      <Link
+        href="/"
+        className={styles.logoLink}
+        aria-label="Go to homepage"
+        prefetch={shouldPrefetch}
+      >
         <Image
           src="/images/logo.png"
           alt="ZNVES logo"
